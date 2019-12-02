df06f210e78d0b9c21e309469e82ce6b054a30e4
David Liu <david@cs.toronto.edu>
2019-12-02 13:01:23 -0500

    patch to disable type inference transformation

--- python_ta/patches/__init__.py
+++ python_ta/patches/__init__.py
@@ -9,7 +9,7 @@ from .error_messages import patch_error_messages
 def patch_all():
     """Execute all patches defined in this module."""
     patch_checkers()
-    patch_type_inference_transform()
+    # patch_type_inference_transform()
     patch_messages()
     patch_linter_transform()
     patch_error_messages()
